0a3ae40f0b7534a9c3ffaba9bae2c129d0ea4d00
pikulatucker@gmail.com <pikulatucker@gmail.com>
2021-02-12 22:53:06 -0800

    100% working

--- script.js
+++ script.js
@@ -3,10 +3,10 @@ document.getElementById('generate').addEventListener('click', event => {
 
   let genPassword = document.getElementById('generate')
   let hasLength = prompt('Pick a character length between 8-128')
-  let hasLowercase = prompt('Do you want lower case letters?')
-  let hasUppercase = prompt('Do you want upper case?')
-  let hasNumeric = prompt('Do you want numbers?')
-  let hasSpecialChar = prompt('Do you want special characters?')
+  let hasLowercase = confirm('Do you want lower case letters?')
+  let hasUppercase = confirm('Do you want upper case?')
+  let hasNumeric = confirm('Do you want numbers?')
+  let hasSpecialChar = confirm('Do you want special characters?')
   let charSet = ''
   let lowercase = 'abcdefghijklmnopqrstuvwxyz'
   let uppercase = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ'
