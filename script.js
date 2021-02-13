b081afad42e343499fd57035b00c4bca5e511ef5
pikulatucker@gmail.com <pikulatucker@gmail.com>
2021-02-12 22:14:31 -0800

    generate password

--- script.js
+++ script.js
@@ -3,15 +3,16 @@ document.getElementById('generate').addEventListener('click', event => {
 
   let genPassword = document.getElementById('generate')
   let hasLength = prompt('Pick a character length between 8-128')
-  let hasLowercase = prompt('Do you want lower case letters?')
-  let hasUppercase = prompt('Do you want upper case?')
-  let hasNumeric = prompt('Do you want numbers?')
-  let hasSpecialChar = prompt('Do you want special characters?')
+  let hasLowercase = prompt('Do you want lower case letters? yes or no')
+  let hasUppercase = prompt('Do you want upper case? yes or no')
+  let hasNumeric = prompt('Do you want numbers? yes or no')
+  let hasSpecialChar = prompt('Do you want special characters? yes or no')
   let charSet = ''
   let lowercase = 'abcdefghijklmnopqrstuvwxyz'
   let uppercase = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ'
   let numbers = '1234567890'
   let symbols = '!@#$%&*'
+  let generatedUsername = ''
 
   if (hasLowercase) {
       charSet += lowercase
@@ -27,8 +28,8 @@ document.getElementById('generate').addEventListener('click', event => {
   }
 
   for (let i = 0; i < hasLength; i++) {
-    charSet[Math.floor(Math.random() * charSet.length)]
+    generatedUsername += charSet[Math.floor(Math.random() * charSet.length)]
   }
-  
+  document.getElementById('password').textContent = generatedUsername
   
 })
